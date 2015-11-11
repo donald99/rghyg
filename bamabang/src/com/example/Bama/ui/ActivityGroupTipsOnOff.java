52e8fa821e7bb6dd9ef919a361ccee59281cd85b
shengwenliu <shengwenliu@meilishuo.com>
2015-11-12 00:00:42 +0800

    m

--- bamabang/src/com/example/Bama/ui/ActivityGroupTipsOnOff.java
+++ bamabang/src/com/example/Bama/ui/ActivityGroupTipsOnOff.java
@@ -7,19 +7,27 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Toast;
+import com.easemob.chat.EMGroupManager;
+import com.easemob.exceptions.EaseMobException;
 import com.example.Bama.R;
+
+import java.util.logging.Handler;
 
 public class ActivityGroupTipsOnOff extends ActivityBase implements View.OnClickListener{
 
     private FrameLayout on_tip,off_tip;
 
-	public static void open(Activity activity){
+    private String groupId = "";
+
+	public static void open(Activity activity,String groupId){
 		Intent intent = new Intent(activity,ActivityGroupTipsOnOff.class);
+        intent.putExtra("groupId",groupId);
 		activity.startActivity(intent);
 	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+        groupId = getIntent().getStringExtra("groupId");
 		setContentView(R.layout.activitiy_group_onoff);
 		super.onCreate(savedInstanceState);
 	}
@@ -49,10 +57,35 @@ public class ActivityGroupTipsOnOff extends ActivityBase implements View.OnClick
                 finish();
                 break;
             case R.id.on_tip:
-                Toast.makeText(this,"on_tip",Toast.LENGTH_LONG).show();
+                Toast.makeText(this,"开启提醒",Toast.LENGTH_LONG).show();
+                /**
+                 * 屏蔽群消息后，就不能接 收到此群的消息 （群创建者不能屏蔽群消息）（还是群里面的成员，但不再接收消息）
+                 * @param groupId， 群id
+                 * @throws EasemobException
+                 */
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            EMGroupManager.getInstance().unblockGroupMessage(groupId);//需异步处理
+                        } catch (EaseMobException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
                 break;
             case R.id.off_tip:
-                Toast.makeText(this,"on_tip",Toast.LENGTH_LONG).show();
+                Toast.makeText(this,"关闭提醒",Toast.LENGTH_LONG).show();
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            EMGroupManager.getInstance().blockGroupMessage(groupId);//需异步处理
+                        } catch (EaseMobException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
                 break;
         }
     }
