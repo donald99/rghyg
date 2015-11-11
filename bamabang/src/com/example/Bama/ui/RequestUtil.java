7312786bffd7de27dd52dd67819ea0958a69f23a
shengwenliu <shengwenliu@meilishuo.com>
2015-11-12 00:02:40 +0800

    m

---
+++ bamabang/src/com/example/Bama/ui/RequestUtil.java
@@ -0,0 +1,67 @@
+package com.example.Bama.ui;
+
+import android.content.Context;
+import android.text.TextUtils;
+import com.example.Bama.background.HCApplication;
+import com.example.Bama.background.config.ServerConfig;
+import com.example.Bama.util.Request;
+import com.example.Bama.util.ToastUtil;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RequestUtil {
+    private RequestUtil mRequestUtil;
+    public RequestUtil getIntance(){
+        if (mRequestUtil==null){
+            mRequestUtil = new RequestUtil();
+            return mRequestUtil;
+        }
+        return mRequestUtil;
+    }
+
+    public static void jubaoGroup(Context context,String fortype,String froid, String jubaoInfo){
+        if (TextUtils.isEmpty(HCApplication.getInstance().getAccount().userId)){
+            ToastUtil.makeLongText("请先登录");
+            return;
+        }
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("uid",HCApplication.getInstance().getAccount().userId));
+        params.add(new BasicNameValuePair("fortype",fortype));
+        params.add(new BasicNameValuePair("froid",froid));
+        params.add(new BasicNameValuePair("msg",jubaoInfo));
+
+        Request.doRequest(context, params, ServerConfig.URL_GET_CONTACT_REPORT, Request.GET, new Request.RequestListener() {
+
+            @Override
+            public void onException(Request.RequestException e) {
+            }
+
+            @Override
+            public void onComplete(String response) {
+
+                if (!TextUtils.isEmpty(response)) {
+                    try {
+                        JSONObject object = new JSONObject(response);
+
+                        if (response.contains("OK")){
+                            ToastUtil.makeLongText("举报成功");
+                        }else{
+                            ToastUtil.makeLongText("举报失败");
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }else{
+                    ToastUtil.makeLongText("举报失败");
+                }
+            }
+        });
+    }
+}
